30a71760b49600edb97c949b91a0e716dcf9d364
Mitchell Warren <liquidvisual@gmail.com>
2014-12-01 13:55:47 +1100

    Small update to gitignore, CNAME and grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -476,8 +476,8 @@ module.exports = function (grunt) {
     //'check',
     //'test',
     'build',
-    'buildcontrol:master'
-    //'buildcontrol:pages',
+    //'buildcontrol:master'
+    'buildcontrol:pages',
     ]);
 
   //=======================================
